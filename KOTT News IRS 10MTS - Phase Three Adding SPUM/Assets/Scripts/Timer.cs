fb41dafb40ab8107bed5c655256354ba1becb18c
JChalant <jc@JCMediaGroup.us>
2025-04-01 15:14:08 -0700

    Finished ground cosmetics and more!
    Ground cosmetics, game boundary colliders, cauldron circle colliders, cannon box colliders, ballista polygon collider works on, but not other. will revisit, after completing background for mass-tilemap collider

--- KOTT News IRS 10MTS - Phase Three Adding SPUM/Assets/Scripts/Timer.cs
+++ KOTT News IRS 10MTS - Phase Three Adding SPUM/Assets/Scripts/Timer.cs
@@ -6,25 +6,28 @@ using TMPro;
 public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
-    [SerializeField] float remainingTime;
-
+    [SerializeField] float CountdownInSecTime;  // 2025 03 31: altered script to accept an argument, allowing for re-use throughout project for various timers concurent or otherwise
+    
     // Update is called once per frame
     void Update()
-    {   
-        if (remainingTime > 0)
-        {
-            remainingTime -= Time.deltaTime;
-        }
-        else if(remainingTime < 0)
-        {
-            remainingTime = 0;
-            // StartInvasion(); &/o GameOver();
-            timerText.color = Color.red;
-        }
+    {
+        float remainingTime = CountdownInSecTime;   // set countdown timer from passed argument
+             
+            if (CountdownInSecTime > 0)                      // countdown
+            {
+                CountdownInSecTime -= Time.deltaTime;
+            }
+            else if (CountdownInSecTime < 0)                  // stop countdown, and do end of timer things. Like; StartInvasion(); &/o GameOver();
+            {
+                CountdownInSecTime = 0;
 
-        int minutes = Mathf.FloorToInt(remainingTime / 60);
-        int seconds = Mathf.FloorToInt(remainingTime % 60);
+                timerText.color = Color.red;
+            }
 
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+            int minutes = Mathf.FloorToInt(CountdownInSecTime / 60);
+            int seconds = Mathf.FloorToInt(CountdownInSecTime % 60);
+
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        
     }
 }
